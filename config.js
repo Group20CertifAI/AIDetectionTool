5b49417bd3dee24d0ec480ef579ce93767bff094
TheGreatCompiler <Andrewbunandrew@gmail.com>
2023-07-18 17:17:57 -0400

    Adding js config files and  updated contact/login

---
+++ config.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCmbhXwRTMiyXl_y1wrPnOY-mipeAMi8T0",
+    authDomain: "certifai-8afc6.firebaseapp.com",
+    projectId: "certifai-8afc6",
+    storageBucket: "certifai-8afc6.appspot.com",
+    messagingSenderId: "1073534950411",
+    appId: "1:1073534950411:web:400de3999bd7730c353b0e",
+    measurementId: "G-BXVF3JMY0N"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth();
