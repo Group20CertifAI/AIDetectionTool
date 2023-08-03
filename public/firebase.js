34b43cddea827b246a336e4d00a57c2665bad1d5
ruchit-p <56946935+ruchit-p@users.noreply.github.com>
2023-08-02 22:55:01 -0400

    Updated Firebase.js and Nav components

--- public/firebase.js
+++ public/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   messagingSenderId: "1073534950411",
   appId: "1:1073534950411:web:400de3999bd7730c353b0e",
   measurementId: "G-BXVF3JMY0N"
-  };
+};
 
 // Initialize Firebase
 const initializeFirebase = () => {
@@ -18,56 +18,48 @@ const initializeFirebase = () => {
 
 initializeFirebase();
 
-
-const signOutButton = document.getElementById('signout');
+// Get all elements
+const loginButtons = document.getElementsByClassName('login');
+const signupButtons = document.getElementsByClassName('signup');
+const signoutButtons = document.getElementsByClassName('signout');
 const userInfo = document.getElementById('user-info');
 
+// Function to display or hide elements
+const displayElements = (elements, display) => {
+  Array.from(elements).forEach(element => {
+    element.style.display = display;
+  });
+}
 
-
-signOutButton.addEventListener('click', async () => {
-  try {
-    await firebase.auth().signOut();
-  } catch (error) {
-    console.log('Error signing out:', error);
-  }
-});
-
+// Auth state listener to get user info and update UI accordingly
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    signInForm.style.display = 'none';
-    signOutButton.style.display = 'block';
+    // User is signed in.
+    displayElements(loginButtons, 'none');
+    displayElements(signupButtons, 'none');
+    displayElements(signoutButtons, 'block');
     userInfo.textContent = `Signed in as ${user.email}`;
   } else {
-    signInForm.style.display = 'block';
-    signOutButton.style.display = 'none';
+    // No user is signed in.
+    displayElements(loginButtons, 'block');
+    displayElements(signupButtons, 'block');
+    displayElements(signoutButtons, 'none');
     userInfo.textContent = '';
   }
 });
-//   Auth state listener to get user info and update UI accordingly
-firebase.auth().onAuthStateChanged((user) => {
-var login = document.getElementById('login');
-var signup = document.getElementById('signup');
-var signout = document.getElementById('signout');
 
-if (user) {
-// User is signed in.
-login.style.display = 'none';
-signup.style.display = 'none';
-signout.style.display = 'block';
-} else {
-// No user is signed in.
-login.style.display = 'block';
-signup.style.display = 'block';
-signout.style.display = 'none';
-}
-});
+// Function to sign out
+const signOut = async (event) => {
+  event.preventDefault();
+  try {
+    await firebase.auth().signOut();
+    console.log('User signed out');
+  } catch (error) {
+    console.error('Error signing out:', error);
+  }
+};
 
-document.getElementById('signout').addEventListener('click', (event) => {
-event.preventDefault();
-firebase.auth().signOut().then(() => {
-console.log('User signed out');
-}).catch((error) => {
-console.error('Error signing out:', error);
+// Add event listeners to sign out buttons
+Array.from(signoutButtons).forEach(button => {
+  button.addEventListener('click', signOut);
 });
-});
-
